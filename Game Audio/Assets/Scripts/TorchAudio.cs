8d0e93a7e71e120d08eb99fdd3e612f2020f023e
JackHodgetts3 <145881042+JackHodgetts3@users.noreply.github.com>
2026-03-03 12:50:09 +0000

    Fire
    -  Started going through FMOD to add a fire sound for the torches

---
+++ Game Audio/Assets/Scripts/TorchAudio.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class TorchAudio : MonoBehaviour
+{
+    public EventReference torchEvent;
+    public GameObject Player;
+
+    private EventInstance torchInstance;
+    public bool isNextToTorch = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        torchInstance = RuntimeManager.CreateInstance(torchEvent);
+        RuntimeManager.AttachInstanceToGameObject(torchInstance, gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(){
+
+        isNextToTorch = true;
+
+        if (isNextToTorch == true) {
+
+            torchInstance.start();
+        }
+    }
+
+    public void OnTriggerExit(){
+        
+        isNextToTorch = false;
+    }
+
+}
